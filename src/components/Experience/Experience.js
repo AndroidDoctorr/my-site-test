6cf0ff249f3c88fda16301256aaf5e0b3bbc54e7
AndroidDoctorr <gbartthenerd@gmail.com>
2022-11-08 00:06:21 -0500

    basic layout of experience component

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -3,6 +3,16 @@ import './Experience.css';
 function Experience() {
     return (
         <div className='experience'>
+            <h3 className='experience-title'></h3>
+            <div className='experience-body'>
+                <div className='experience-color'></div>
+                <div className='experience-roles'>
+                    <h4 className='experience-role'></h4>
+                    <ul className='experience-duties'></ul>
+                </div>
+                <div className='experience-date'></div>
+            </div>
+
         </div>
     );
 }
