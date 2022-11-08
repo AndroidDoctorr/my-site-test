a67af0d56d86cfcd75a7a828f71eefad172f1c58
AndroidDoctorr <gbartthenerd@gmail.com>
2022-11-08 01:01:16 -0500

    mapped role duties

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -7,7 +7,11 @@ function Role(props) {
                 <h4 className='role-title'>
                     {props.role.title}
                 </h4>
-                <ul className='role-duties'></ul>
+                <ul className='role-duties'>
+                    {props.role.duties.map(
+                        duty => { return <li>{duty}</li> }
+                    )}
+                </ul>
             </div>
             <div className='role-date'>
                 {props.role.dates}
