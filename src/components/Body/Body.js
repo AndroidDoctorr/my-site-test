9c0474b22f85495e607b88146a1af0cf32fe5e85
AndroidDoctorr <gbartthenerd@gmail.com>
2022-11-07 23:48:21 -0500

    import data into body for use in Sections

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,6 +3,7 @@ import Section from '../Section/Section';
 import Experience from '../Experience/Experience';
 import Certification from '../Certification/Certification';
 import BulletItems from '../BulletItems/BulletItems';
+import data from '../../data';
 
 function Body() {
     return (
@@ -10,22 +11,27 @@ function Body() {
             <Section
                 title={"Professional Experience"}
                 template={Experience}
+                data={data.experience}
             />
             <Section
                 title={"Education"}
                 template={Experience}
+                data={data.education}
             />
             <Section
                 title={"Certifications"}
                 template={Certification}
+                data={data.certifications}
             />
             <Section
                 title={"Personal Projects"}
                 template={Experience}
+                data={data.personalProjects}
             />
             <Section
                 title={"Skills/Technologies"}
                 template={BulletItems}
+                data={data.skills}
             />
         </div>
     );
---
+++ src/data/index.js
@@ -0,0 +1,14 @@
+
+import experience from '../../data/experience.json';
+import education from '../../data/education.json';
+import certifications from '../../data/certifications.json';
+import personalProjects from '../../data/personal-projects.json';
+import skills from '../../data/skills.json';
+
+export default {
+    experience,
+    education,
+    certifications,
+    personalProjects,
+    skills
+}
