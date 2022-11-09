5c4a2b418def1135adde3e952f7105dbb9f732a8
AndroidDoctorr <gbartthenerd@gmail.com>
2022-11-08 23:07:15 -0500

    added variable for data export

--- src/data/index.js
+++ src/data/index.js
@@ -1,13 +1,15 @@
-import experience from './experience.json';
-import education from './education.json';
-import certifications from './certifications.js';
-import personalProjects from './personal-projects.json';
-import skills from './skills.json';
+import experience from './experience.json'
+import education from './education.json'
+import certifications from './certifications.js'
+import personalProjects from './personal-projects.json'
+import skills from './skills.json'
 
-export default {
+const data = {
     experience,
     education,
     certifications,
     personalProjects,
     skills
 }
+
+export default data
