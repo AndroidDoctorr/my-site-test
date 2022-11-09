3072e8aaa335494fb7fc11473a3dfbe35d9f9122
AndroidDoctorr <gbartthenerd@gmail.com>
2022-11-08 23:23:18 -0500

    added github and linkedin links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import { useContext } from 'react'
 import { ColorThemeContext } from '../context/colorThemeContext'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import classNames from 'classnames'
 import basicInfo from '../data/basic-info.json'
 
@@ -9,7 +11,23 @@ function Header() {
 	return (
 		<div className={classNames('header', theme.color)} >
 			<div className='header-info'>
-				<h1>{basicInfo.name}</h1>
+				<h1>
+					{basicInfo.name}
+					<a
+						href={basicInfo.linkedin}
+						target="_blank"
+                    	rel="noopener noreferrer"
+					>
+						<FontAwesomeIcon icon={faLinkedin} />
+					</a>
+					<a
+						href={basicInfo.github}
+						target="_blank"
+                    	rel="noopener noreferrer"
+					>
+						<FontAwesomeIcon icon={faGithub} />
+					</a>
+				</h1>
 				<div>{basicInfo.email}</div>
 				<div>{basicInfo.phone}</div>
 			</div>
