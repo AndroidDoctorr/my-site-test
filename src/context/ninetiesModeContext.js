61cd722c37deae4cb776dd3881e58cb9113a4758
AndroidDoctorr <gbartthenerd@gmail.com>
2022-11-08 22:43:15 -0500

    created nineties mode context

---
+++ src/context/ninetiesModeContext.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from 'react';
+
+export const NinetiesModeContext = createContext();
+
+export const NinetiesModeContextProvider = (props) => {
+    const [isNinetiesMode, setNinetiesMode] = useState(false)
+
+    const toggleNinetiesMode = () => {
+        setNinetiesMode(!isNinetiesMode)
+    }
+    
+    return (
+        <NinetiesModeContext.Provider value={{ isNinetiesMode, toggleNinetiesMode }}>
+            {props.children}
+        </NinetiesModeContext.Provider>
+    )
+}
