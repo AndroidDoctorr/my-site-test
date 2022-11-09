91412113e99043866e186f8d27d8f97ec192371b
AndroidDoctorr <gbartthenerd@gmail.com>
2022-11-08 21:57:39 -0500

    added urls to certification data

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -2,22 +2,26 @@ const certifications = [
     {
         "title": "AWS Cloud Practitioner",
         "date": "Aug 2022",
-        "image": require("../assets/logos/aws-cp.png")
+        "image": require("../assets/logos/aws-cp.png"),
+        "url": "https://aws.amazon.com/certification/certified-cloud-practitioner/"
     },
     {
         "title": "MTA 98-361 - Software Development Fundamentals",
         "date": "Jun 2021",
-        "image": require("../assets/logos/mta-sd.png")
+        "image": require("../assets/logos/mta-sd.png"),
+        "url": "https://learn.microsoft.com/en-us/certifications/exams/98-361"
     },
     {
         "title": "CIW JavaScript Specialist",
         "date": "Apr 2021",
-        "image": require("../assets/logos/ciw-jss.png")
+        "image": require("../assets/logos/ciw-jss.png"),
+        "url": "https://www.ciwcertified.com/ciw-certifications/web-development-series/javascript-specialist"
     },
     {
         "title": "IASCC Green Belt",
         "date": "Dec 2020",
-        "image": require("../assets/logos/lss-gb.png")
+        "image": require("../assets/logos/lss-gb.png"),
+        "url": "https://iassc.org/six-sigma-certification/green-belt-certification/"
     }
 ];
 
